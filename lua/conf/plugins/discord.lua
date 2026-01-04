1e644e2c961effe7557e59adbbe67f7c068a47a7
CompileRider <esmilviciosomercado@gmail.com>
2026-01-04 16:58:11 -0400

    🎮 Use Bare Metal Discord app

--- lua/conf/plugins/discord.lua
+++ lua/conf/plugins/discord.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   opts = {
     editor = {
-      client = "neovim",
+      client = "1457478127566459078",
       tooltip = "touching bare metal",
       icon = nil,
     },
