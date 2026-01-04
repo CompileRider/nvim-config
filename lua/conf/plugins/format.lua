35dc187386a0a224af6642a6c1aefaf3dc5e5b6e
CompileRider <esmilviciosomercado@gmail.com>
2026-01-04 14:13:51 -0400

    🐧 Official Linux kernel clang-format style
    Based on torvalds/linux/.clang-format:
    - IndentWidth: 8, TabWidth: 8, UseTab: Always
    - BreakBeforeBraces: Linux
    - ColumnLimit: 80
    - SpaceAfterCStyleCast: false (kernel style)
    - AlignAfterOpenBracket: Align
    - IndentCaseLabels: false
    - ContinuationIndentWidth: 8
    - No short functions/if/loops on single line

--- lua/conf/plugins/format.lua
+++ lua/conf/plugins/format.lua
@@ -14,7 +14,33 @@ return {
     formatters = {
       clang_format = {
         prepend_args = {
-          "--style={BasedOnStyle: LLVM, IndentWidth: 8, TabWidth: 8, UseTab: Always, BreakBeforeBraces: Linux, AllowShortFunctionsOnASingleLine: None, AllowShortIfStatementsOnASingleLine: false, IndentCaseLabels: false, ColumnLimit: 80, PointerAlignment: Right, SpaceAfterCStyleCast: true, AlignAfterOpenBracket: DontAlign, AlignOperands: DontAlign, AlignTrailingComments: false, AllowAllParametersOfDeclarationOnNextLine: false, BinPackParameters: true, BinPackArguments: true}",
+          "--style={"
+            .. "BasedOnStyle: LLVM, "
+            .. "IndentWidth: 8, "
+            .. "TabWidth: 8, "
+            .. "UseTab: Always, "
+            .. "ColumnLimit: 80, "
+            .. "BreakBeforeBraces: Linux, "
+            .. "AllowShortFunctionsOnASingleLine: None, "
+            .. "AllowShortIfStatementsOnASingleLine: false, "
+            .. "AllowShortLoopsOnASingleLine: false, "
+            .. "AllowShortBlocksOnASingleLine: false, "
+            .. "IndentCaseLabels: false, "
+            .. "IndentGotoLabels: false, "
+            .. "PointerAlignment: Right, "
+            .. "SpaceAfterCStyleCast: false, "
+            .. "SpaceBeforeParens: ControlStatementsExceptForEachMacros, "
+            .. "AlignAfterOpenBracket: Align, "
+            .. "AlignTrailingComments: false, "
+            .. "AlignConsecutiveAssignments: false, "
+            .. "AlignConsecutiveDeclarations: false, "
+            .. "AlignOperands: true, "
+            .. "BinPackArguments: true, "
+            .. "BinPackParameters: true, "
+            .. "ContinuationIndentWidth: 8, "
+            .. "ReflowComments: false, "
+            .. "SortIncludes: false"
+            .. "}",
         },
       },
     },
