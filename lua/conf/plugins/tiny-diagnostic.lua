951bd05f6daa92a37c210f03f139e423a12de156
CompileRider <esmilviciosomercado@gmail.com>
2026-01-07 21:53:25 -0400

    Diagnostics: show icon only, expand on cursor
    - Hide message text by default (just icon)
    - Full formatted message appears when cursor is on line
    - Multiline support enabled

--- lua/conf/plugins/tiny-diagnostic.lua
+++ lua/conf/plugins/tiny-diagnostic.lua
@@ -1,4 +1,4 @@
--- Inline diagnostics with modern styling
+-- Inline diagnostics: icon only, expand on cursor
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "LspAttach",
@@ -8,11 +8,17 @@ return {
       preset = "modern",
       transparent_bg = true,
       options = {
-        multilines = { enabled = true },
         show_source = { enabled = true },
+        multilines = { enabled = true, always_show = false },
         show_all_diags_on_cursorline = true,
         overflow = { mode = "wrap" },
-        throttle = 50,
+        throttle = 20,
+        softwrap = 50,
+        add_messages = {
+          messages = false,           -- Hide messages by default
+          display_count = false,      -- Just show icon
+          show_multiple_glyphs = true,
+        },
       },
     })
     vim.diagnostic.config({ virtual_text = false })
