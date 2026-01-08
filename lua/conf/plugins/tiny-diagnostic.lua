2b919b1e9518bc37ae8ebe7c2be2068f98294267
CompileRider <esmilviciosomercado@gmail.com>
2026-01-07 22:04:34 -0400

    Diagnostic signs: use letters (E W I H) instead of icons

--- lua/conf/plugins/tiny-diagnostic.lua
+++ lua/conf/plugins/tiny-diagnostic.lua
@@ -21,6 +21,16 @@ return {
         },
       },
     })
-    vim.diagnostic.config({ virtual_text = false, signs = false })
+    vim.diagnostic.config({
+      virtual_text = false,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "E",
+          [vim.diagnostic.severity.WARN] = "W",
+          [vim.diagnostic.severity.INFO] = "I",
+          [vim.diagnostic.severity.HINT] = "H",
+        },
+      },
+    })
   end,
 }
